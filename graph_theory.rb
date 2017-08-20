21ca802603429543ec4f7596d1874a99f83ce298
Vitaly Liber <liber@sibext.com>
2017-08-20 16:02:28 +0700

    first commit

---
+++ graph_theory.rb
@@ -0,0 +1,79 @@
+require 'json'
+
+class GraphTheory
+
+  attr_accessor :vertexes
+
+  def initialize
+    self.vertexes = []
+  end
+
+  def create_vertex name, value
+    vertexes_validation_uniqueness value
+    vertexes << {name: name, value: value, edges: []}
+    ({status: 201, vertexes: vertexes})
+  rescue => e
+    ({status: 401, message: e.message})
+  end
+
+  def create_edge name, from, to
+    vertexes_validation_present to
+    vs = vertexes.map{|el| el.dup}
+    vs.map! do |el|
+      if el[:value] == from
+        if el[:value] != to
+          if el[:edges].count <= 1
+            if not el[:edges].include? to
+              el.merge({edges: el[:edges].dup << to})
+            else
+              raise 'Vertex has already had the same edge'
+            end
+          else
+            raise 'Vertex has already had two edges'
+          end
+        else
+          raise 'Edge shouldn\'t have the same value as vertex'
+        end
+      else
+        el
+      end
+    end
+
+    edges_validation_uniqueness vs, from
+    self.vertexes = vs
+  rescue => e
+    ({status: 401, message: e.message})
+  end
+
+  def vertexes_validation_uniqueness value
+    raise "Graph has already had the vertex: #{value}" if vertexes.any? do |vertex|
+      vertex[:value] == value
+    end
+  end
+
+  def vertexes_validation_present value
+    raise "Graph has not had the vertex: #{value}" unless vertexes.any? do |vertex|
+      vertex[:value] == value
+    end
+  end
+
+  def edges_validation_uniqueness vertexes, from
+    red_vertexes = []
+    es = [from]
+
+    while es.any? do
+      new_edges = []
+      es.each do |el|
+        new_edges.push *vertexes.find {|v| v[:value] == el }&.dig(:edges)
+        red_vertexes.push *new_edges
+        unless red_vertexes.uniq.length == red_vertexes.length
+          es = []
+          raise "Validator has find a cycle. Please choose an other vertexes"
+        end
+      end
+      es = new_edges
+    end
+
+  end
+
+end
---
+++ spec/graph_theory_spec.rb
@@ -0,0 +1,116 @@
+require "./graph_theory"
+
+describe GraphTheory do
+  describe ".create_vertex" do
+
+    context "vertexes tests" do
+
+      it "should add new vertexes" do
+        graph_theory = GraphTheory.new
+        expect(graph_theory.create_vertex 'first', 1).to eq({:status=>201, :vertexes=>[{:name=>"first", :value=>1, :edges=>[]}]})
+        expect(graph_theory.create_vertex 'second', 2).to eq({:status=>201, :vertexes=>[
+            {:name=>"first", :value=>1, :edges=>[]},
+            {:name=>"second", :value=>2, :edges=>[]}
+        ]})
+
+      end
+
+      it "should't have a second vertex with the same value" do
+        graph_theory = GraphTheory.new
+        expect(graph_theory.create_vertex 'first', 1).to eq({:status=>201, :vertexes=>[{:name=>"first", :value=>1, :edges=>[]}]})
+        expect(graph_theory.create_vertex 'second', 1).to eq({:status=>401, :message=>"Graph has already had the vertex: 1"})
+      end
+
+    end
+
+    context "edges tests" do
+      it "should add new edges" do
+        graph_theory = GraphTheory.new
+        expect(graph_theory.create_vertex 'first vertex', 1)
+        expect(graph_theory.create_vertex 'second vertex', 2)
+        expect(graph_theory.create_vertex 'third vertex', 3)
+
+        expect(graph_theory.create_edge 'first edge', 1, 2).to eq([
+                                                                      {:name=>"first vertex", :value=>1, :edges=>[2]},
+                                                                      {:name=>"second vertex", :value=>2, :edges=>[]},
+                                                                      {:name=>"third vertex", :value=>3, :edges=>[]}
+                                                                  ])
+        expect(graph_theory.create_edge 'second vertex', 2, 3).to eq(
+                                                                      [
+                                                                          {:name=>"first vertex", :value=>1, :edges=>[2]},
+                                                                          {:name=>"second vertex", :value=>2, :edges=>[3]},
+                                                                          {:name=>"third vertex", :value=>3, :edges=>[]}
+                                                                      ])
+      end
+
+      it "shouldn't have the same value as vertex" do
+        graph_theory = GraphTheory.new
+        expect(graph_theory.create_vertex 'first vertex', 1)
+        expect(graph_theory.create_vertex 'second vertex', 2)
+        expect(graph_theory.create_vertex 'third vertex', 3)
+
+        expect((graph_theory.create_edge 'first edge', 1, 2).first).to eq({:name=>"first vertex", :value=>1, :edges=>[2]})
+        expect(graph_theory.create_edge 'second edge', 1, 2).to eq(
+                                                                      {
+                                                                          :status=>401,
+                                                                          :message=>"Vertex has already had the same edge"
+                                                                      }
+                                                                  )
+      end
+
+      it "shouldn't have more than two edges" do
+        graph_theory = GraphTheory.new
+        expect(graph_theory.create_vertex 'first vertex', 1)
+        expect(graph_theory.create_vertex 'second vertex', 2)
+        expect(graph_theory.create_vertex 'third vertex', 3)
+        expect(graph_theory.create_vertex 'fourth vertex', 4)
+
+        expect((graph_theory.create_edge 'first edge', 1, 2).first).to eq({:name=>"first vertex", :value=>1, :edges=>[2]})
+
+        expect((graph_theory.create_edge 'second edge', 1, 3).first).to eq({:name=>"first vertex", :value=>1, :edges=>[2, 3]})
+
+        expect(graph_theory.create_edge 'third edge', 1, 4).to eq({:status=>401, :message=>"Vertex has already had two edges"})
+      end
+
+      it "shouldn't have the same edge" do
+        graph_theory = GraphTheory.new
+        expect(graph_theory.create_vertex 'first vertex', 1)
+        expect(graph_theory.create_vertex 'second vertex', 2)
+
+
+        expect((graph_theory.create_edge 'first edge', 1, 2).first).to eq({:name=>"first vertex", :value=>1, :edges=>[2]})
+
+        expect(graph_theory.create_edge 'second vertex', 1, 2).to eq({:status=>401, :message=>"Vertex has already had the same edge"})
+      end
+
+      it "shouldn't have more than two edges" do
+        graph_theory = GraphTheory.new
+        expect(graph_theory.create_vertex 'first vertex', 1)
+        expect(graph_theory.create_vertex 'second vertex', 2)
+        expect(graph_theory.create_vertex 'third vertex', 3)
+        expect(graph_theory.create_vertex 'fourth vertex', 4)
+
+        expect((graph_theory.create_edge 'first edge', 1, 2).first).to eq({:name=>"first vertex", :value=>1, :edges=>[2]})
+
+        expect((graph_theory.create_edge 'second edge', 1, 3).first).to eq({:name=>"first vertex", :value=>1, :edges=>[2, 3]})
+
+        expect(graph_theory.create_edge 'third edge', 1, 4).to eq({:status=>401, :message=>"Vertex has already had two edges"})
+      end
+
+      it "shouldn't have a cycle" do
+        graph_theory = GraphTheory.new
+        expect(graph_theory.create_vertex 'first vertex', 1)
+        expect(graph_theory.create_vertex 'second vertex', 2)
+        expect(graph_theory.create_vertex 'third vertex', 3)
+
+        expect((graph_theory.create_edge 'first edge', 1, 2).first).to eq({:name=>"first vertex", :value=>1, :edges=>[2]})
+
+        expect((graph_theory.create_edge 'second edge', 2, 3)[1]).to eq({:name=>"second vertex", :value=>2, :edges=>[3]})
+
+        expect(graph_theory.create_edge 'third edge', 3, 1).to eq({:status=>401, :message=>"Validator has find a cycle. Please choose an other vertexes"})
+      end
+
+    end
+
+  end
+end
